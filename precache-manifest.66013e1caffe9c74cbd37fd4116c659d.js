f5ed2263ca8c9b907b6a464fab8f27d8ad3d508a
Dyldinski <dubois.a.dylan@gmail.com>
2019-02-18 20:12:29 -0600

    Updates

---
+++ precache-manifest.66013e1caffe9c74cbd37fd4116c659d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4797d12f54269babec06",
+    "url": "/Teachirp/static/js/runtime~main.4797d12f.js"
+  },
+  {
+    "revision": "e2841a635b2f78cd52ab",
+    "url": "/Teachirp/static/js/main.e2841a63.chunk.js"
+  },
+  {
+    "revision": "9d017d42b848b833dbb5",
+    "url": "/Teachirp/static/js/1.9d017d42.chunk.js"
+  },
+  {
+    "revision": "e2841a635b2f78cd52ab",
+    "url": "/Teachirp/static/css/main.1183d1ad.chunk.css"
+  },
+  {
+    "revision": "9d017d42b848b833dbb5",
+    "url": "/Teachirp/static/css/1.da646582.chunk.css"
+  },
+  {
+    "revision": "db22e0760c9a45909cf3f11568620550",
+    "url": "/Teachirp/index.html"
+  }
+];
