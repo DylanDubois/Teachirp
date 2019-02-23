e46064a8c92bce9ed18ee89f5f55f540bc208059
Dyldinski <dubois.a.dylan@gmail.com>
2019-02-23 16:40:07 -0600

    Updates

---
+++ precache-manifest.70e38fd2abf2bf9bfa886428d5c28a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9954f40aefec2ff14a0d",
+    "url": "/static/js/main.9954f40a.chunk.js"
+  },
+  {
+    "revision": "d5d24961dd35ea3842c5",
+    "url": "/static/js/1.d5d24961.chunk.js"
+  },
+  {
+    "revision": "9954f40aefec2ff14a0d",
+    "url": "/static/css/main.59b4b427.chunk.css"
+  },
+  {
+    "revision": "d5d24961dd35ea3842c5",
+    "url": "/static/css/1.c4f664a6.chunk.css"
+  },
+  {
+    "revision": "0e513eedad284f2510528f6495b817b6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Teachirp/precache-manifest.2583dd21be068e1f6cf39f5c673252ae.js"
+  "/precache-manifest.70e38fd2abf2bf9bfa886428d5c28a97.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Teachirp/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
